98d209cedc1861a3efe3dc13e29cd950d45fce30
aajcs <98472648+aajcs@users.noreply.github.com>
2025-08-01 09:50:13 -0400

    Actualizando la plantilla de Vercel y el primer hola mundo de la pagina

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '4rem' }}
+    >
+      <h1
+        style={{
+          fontSize: '3rem',
+          color: '#0070f3',
+          fontFamily: 'Comic Sans MS, cursive, sans-serif',
+        }}
+      >
+        👋 ¡Hola Mundo!
+      </h1>
+      <p style={{ fontSize: '2rem', color: '#333', marginTop: '1rem' }}>Castillo IT Systems 🚀</p>
+    </main>
+  );
+}
